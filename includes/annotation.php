dfc1fdd4664bbe51f59a6de80238bfbe89697b4a
Greg Tunink <techgique@gmail.com>
2016-09-16 17:23:23 -0500

    Add display of referenced annotations
    Signify an annotation as an annotation reference
    Load referenced annotation and display as contents with link to edit

--- includes/annotation.php
+++ includes/annotation.php
@@ -22,6 +22,19 @@
 
   <!-- Annotation content -->
   <div class="col-md-8">
+    <?php
+      $anno_ref = (isset($anno["anno_ref_id"]) && $anno["anno_ref_id"] !== "") ? true : false;
+
+      if ($anno_ref) {
+        echo <<<END
+    <h4 class="pull-right">Annotation Reference</h4>
+END;
+
+        # Retrieve referenced annotation
+        $ref_anno_json = get_annotation_by_id($anno["anno_ref_id"]);
+        $ref_anno = json_decode($ref_anno_json, true);
+      }
+    ?>
     <h5>
       Highlight:
       <span class="quote"><?php echo $anno["quote"]?></span>
@@ -39,7 +52,13 @@
 </div>
 <span class="text">Alt Text "$2"</span>
 END
-          , $anno["text"]
+          , ($anno_ref)
+            ? <<<END
+<h5>Annotation $ref_anno[id]:</h5>
+$ref_anno[text]
+<a href="/annotonia_status/edit.php?id=$ref_anno[id]">&gt;&gt; Edit Referenced Annotation</a>
+END
+            : $anno["text"]
         );
       ?>
     </div>
