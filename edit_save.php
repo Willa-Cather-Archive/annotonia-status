d2b4edc0ce26e2e734ae39bb2ec3f732a6e9b784
Jessica Dussault <jdussault4@gmail.com>
2016-09-16 11:27:43 -0500

    moves annotation row to separate file

--- edit_save.php
+++ edit_save.php
@@ -2,7 +2,7 @@
   include "env/config.php";
   include "lib/helpers.php";
 
-  // get the annotation info
+  // get the original annotation info
   $curl = curl_init();
   $url = $flask_url."/annotations/".$_POST["id"];
   $url = str_replace(" ", "%20", $url);
@@ -13,12 +13,13 @@
   $res = curl_exec($curl); 
   curl_close($curl);
 
+  // add in the fields the user may have altered
   $anno = json_decode($res, true);
   $anno["text"] = $_POST["text"];
   $anno["tags"] = array($_POST["tags"]);
-
   $anno_update = json_encode($anno);
 
+  // PUT a request to update the annotation
   $puturl = $flask_url."/annotations/".$_POST["id"];
   $put = curl_init();
   curl_setopt($put, CURLOPT_URL, $puturl);
@@ -36,70 +37,13 @@
   <body>
     <div class="container">
       <?php include "layout/navbar.php"; ?>
-
-      <h2>Updated annotation: </h3>
       
-      <?php
-        $tag_html = generate_tags($anno["tags"]);
-      ?>
-      <div>
-        <div class="row">
+      <h2>Updated annotation: </h3>
+      <?php  $tag_html = generate_tags($anno["tags"]); ?>
+      <?php include "includes/annotation.php" ?>
 
-          <!-- Identification and Links -->
-          <div class="col-md-3">
-            <h5>Letter: <?php echo $anno["pageID"] ?></h5>
-            <div class="pull-right">
-              <form action="<?php echo $status_link_url?>/edit.php">
-                <input type="hidden" name="id" value="<?php echo $anno['id']?>"/>
-                <input class="form-control edit" type="submit" value="Edit"/>
-              </form>
-              <?php echo $tag_html ?>
-            </div>
-            <p>ID: <?php echo $anno["id"] ?></p>
-            <?php if (isset($anno["pageID"])): ?>
-              <a href="<?php echo $boilerplate_url?><?php echo $anno["pageID"]?>.html">Annotate</a>
-                 | 
-                <a href="<?php echo $catherletters_url?><?php echo $anno["pageID"]?>.html">Cather View</a>
-            <?php else: ?>
-              No links available for nonexistent id
-            <?php endif; ?>
-          </div>
-
-          <!-- Annotation content -->
-          <div class="col-md-8">
-            <h5>
-              Highlight:
-              <span class="quote"><?php echo $anno["quote"]?></span>
-            </h5>
-            <div class="well well-sm">
-              <?php
-                # Wrap image with link, limit size, display alt text
-                echo preg_replace(
-                  '/<img src="(.+?)" alt="(.*?)"(.*)?>/'
-                  , <<<END
-<div class="container-fluid">
-  <a href="$1">
-    <img class="img-responsive" src="$1" alt="$2" title="$2"$3>
-  </a>
-</div>
-<span class="text">Alt Text "$2"</span>
-END
-                  , $anno["text"]
-                );
-              ?>
-            </div>
-          </div> <!-- /div col-md-8 -->
-
-          <!-- Delete Annotation -->
-          <div class="col-md-1">
-            <form action="<?php echo $status_link_url?>/delete.php">
-              <input type="hidden" name="id" value="<?php echo $anno['id']?>"/>
-              <input class="form-control delete" type="submit" value="Delete"/>
-            </form>
-          </div>
-        </div> <!-- /div col-md-8 -->
-      </div>
       <a href="<?php echo $status_link_url ?>">Back to Status View</a>
+
       <h2>Status: </h2>
       <?php echo print_r($putres) ?>
     </div>
--- index.php
+++ index.php
@@ -43,66 +43,11 @@
         <div>
           <?php for ($i = 0; $i < $anno_length; $i++): ?>
             <?php
-              $row = $annotations["rows"][$i];
-              $tag_html = generate_tags($row["tags"]);
+              $anno = $annotations["rows"][$i];
+              $tag_html = generate_tags($anno["tags"]);
             ?>
             <div>
-              <div class="row">
-
-                <!-- Identification and Links -->
-                <div class="col-md-3">
-                  <h5>Letter: <?php echo $row["pageID"] ?></h5>
-                  <div class="pull-right">
-                    <form action="<?php echo $status_link_url?>/edit.php">
-                      <input type="hidden" name="id" value="<?php echo $row['id']?>"/>
-                      <input class="form-control edit" type="submit" value="Edit"/>
-                    </form>
-                    <?php echo $tag_html ?>
-                  </div>
-                  <p>ID: <?php echo $row["id"] ?></p>
-                  <?php if (isset($row["pageID"])): ?>
-                    <a href="<?php echo $boilerplate_url?><?php echo $row["pageID"]?>.html">Annotate</a>
-                       | 
-                      <a href="<?php echo $catherletters_url?><?php echo $row["pageID"]?>.html">Cather View</a>
-                  <?php else: ?>
-                    No links available for nonexistent id
-                  <?php endif; ?>
-                </div>
-
-                <!-- Annotation content -->
-                <div class="col-md-8">
-                  <h5>
-                    Highlight:
-                    <span class="quote"><?php echo $row["quote"]?></span>
-                  </h5>
-                  <div class="well well-sm">
-                    <?php
-                      # Wrap image with link, limit size, display alt text
-                      echo preg_replace(
-                        '/<img src="(.+?)" alt="(.*?)"(.*)?>/'
-                        , <<<END
-<div class="container-fluid">
-  <a href="$1">
-    <img class="img-responsive" src="$1" alt="$2" title="$2"$3>
-  </a>
-</div>
-<span class="text">Alt Text "$2"</span>
-END
-                        , $row["text"]
-                      );
-                    ?>
-                  </div>
-                </div> <!-- /div col-md-8 -->
-
-                <!-- Delete Annotation -->
-                <div class="col-md-1">
-                  <form action="<?php echo $status_link_url?>/delete.php">
-                    <input type="hidden" name="id" value="<?php echo $row['id']?>"/>
-                    <input class="form-control delete" type="submit" value="Delete"/>
-                  </form>
-                </div>
-              </div> <!-- /div col-md-8 -->
-            </div>
+            <?php include "includes/annotation.php" ?>
             <hr>
           <?php endfor ?>
         </div>
