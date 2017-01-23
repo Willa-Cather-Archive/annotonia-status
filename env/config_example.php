4c617b74e0b1c4c814b9356d786fac1f451596c2
Greg Tunink <techgique@gmail.com>
2017-01-22 19:27:09 -0600

    Generalize config paths; Add basic favicon
    Remove protocol and hostname from config paths to use relative paths,
    so they don't break when moving between servers
    
    Flask URL must still have full hostname as cURL argument though
    
    Ceiling cat is watching you annotate

--- env/config_example.php
+++ env/config_example.php
@@ -1,7 +1,7 @@
 <?php
-  $boilerplate_url = "https://server.unl.edu/annotonia/tei/tei-boilerplate/cat.";
-  $catherletters_url = "http://cather.unl.edu/letters/";
-  $status_link_url = "https://server.unl.edu/annotonia_status";
+  $boilerplate_url = "/annotonia/tei/tei-boilerplate/cat.";
+  $catherletters_url = "/letters/";
+  $status_link_url = "/annotonia_status";
 
   $catherletters_dir = "/path/to/cocoon/annotonia/xml/letters/";
   $flask_url = "http://server.unl.edu:port";
